3c5123336545e6eb461cc02df3d313142d3a6487
Patricia Bardowell <patricia.bardowell@gmail.com>
2017-10-20 01:33:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ var height = "74"
 if (name === "Susan") {
   console.log("The name is correct")
 }
-else {
-  console.log("Expected the name to be Susan")
-}
 
 // Don't worry about this
 module.exports = { name, height
